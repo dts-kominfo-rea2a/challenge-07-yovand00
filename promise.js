3c2aae5cba7b4636263f0b735ff42ce3b3b81479
chicken on fire <106462448+yovand00@users.noreply.github.com>
2022-06-12 23:47:08 +0700

    challenge-07-Yesaya Yova

--- promise.js
+++ promise.js
@@ -1,8 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (emosi) => {
+  let promiseIXX = await promiseTheaterIXX();
+  let promiseVGC = await promiseTheaterVGC();
+  let gabung = promiseIXX.concat(promiseVGC);
+  let totalEmosi = 0;
+  gabung.map(promiseIXX => promiseIXX.hasil === emosi && (totalEmosi += 1) )
+  return totalEmosi
+};
+// console.log()
+// promiseOutput("marah")
 module.exports = {
   promiseOutput,
 };
